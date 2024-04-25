e1e78a42f152a4f3edced01bf34ede04c6830070
Junhao Wang <junhaowanggg@gmail.com>
2024-04-24 22:46:19 -0700

    Add timer

---
+++ core/timer.h
@@ -0,0 +1,38 @@
+//
+// Created by Junhao Wang (@forkercat) on 4/24/24.
+//
+
+#pragma once
+
+#include "core/typedefs.h"
+
+#include <chrono>
+
+namespace mapo
+{
+	class Timer
+	{
+	public:
+		Timer() = default;
+
+		void Reset()
+		{
+			m_start = std::chrono::high_resolution_clock::now();
+		}
+
+		F32 GetSeconds() const
+		{
+			using namespace std::chrono;
+			return duration_cast<nanoseconds>(high_resolution_clock::now() - m_start).count() * 0.001f * 0.001f * 0.001f;
+		}
+
+		F32 GetMilliseconds() const
+		{
+			return GetSeconds() * 1000.0f;
+		}
+
+	private:
+		std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
+	};
+	
+} // namespace mapo
